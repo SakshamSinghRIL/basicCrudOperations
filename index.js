111041c446175a01a0c8f8465595aaccace87324
Saksham Singh <sakshamsinghril>
2024-05-03 11:58:46 +0530

    csv file upload

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const oracledb = require("oracledb");
 const amqp = require("amqplib/callback_api");
 const dbConfig = require("./configs/dbconfig");
 const client = require("./configs/redisconfig");
+const path = require("path")
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const fs = require('fs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
+
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"))
 
 const RABBITMQ_URL = "amqp://localhost";
 const QUEUE_NAME = "customer_queue";
@@ -18,7 +25,54 @@ const QUEUE_NAME = "customer_queue";
 //   queue: "customer_queue",
 // };
 
-app.get("/", (req, res) => {
+app.get('/fileupload',(req ,res)=>{
+  return res.render("homepage");
+
+
+})
+
+app.post('/upload', upload.single('csvFile'), async (req, res) => {
+  const file = req.file;
+  const filePath = file.path;
+
+  try {
+    
+      const data = fs.readFileSync(filePath, 'utf8');
+
+
+      const rows = data.split('\n').map(row => row.split(','));
+      console.log(rows.length)
+      console.log(rows)
+      const connection = await oracledb.getConnection(dbConfig);
+      var i;
+     for(i=0;i<=rows.length-2;i++){
+      await connection.execute(
+        `INSERT INTO SEFTTXTEST6.CUSTOMERSDATASELECTION (CUSTOMER_ID,FIRST_NAME,LAST_NAME,GENDER) VALUES (:id, :firstname, :lastname, :gender)`,
+        {
+          id: rows[i][0],
+          firstname: rows[i][1],
+          lastname: rows[i][2],
+          gender: rows[i][3]
+        },
+        { autoCommit: true }
+      );
+   
+    }
+
+      await connection.close();
+
+      res.status(200).send('File uploaded and data stored successfully.');
+  } catch (err) {
+      console.error(err);
+      res.status(500).send('Error uploading file.');
+  } finally {
+      // Delete uploaded file
+      fs.unlinkSync(filePath);
+  }
+});
+
+
+app.get("/welcome", (req, res) => {
   res.send("Welcome to my Crud Operations server");
 });
 
